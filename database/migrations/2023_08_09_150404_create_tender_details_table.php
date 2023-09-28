dc9a78ea459321901421f142f6347a0c72ce1f5b
Loai Dev <loaidev64@gmail.com>
2023-09-28 15:00:26 +0300

    trender details

--- database/migrations/2023_08_09_150404_create_tender_details_table.php
+++ database/migrations/2023_08_09_150404_create_tender_details_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
 
         Schema::create('tender_details', function (Blueprint $table) {
             $table->id();
-            $table->string('type');
+            $table->string('name');
             $table->string('data');
             $table->foreignId('tender_id')->constrained();
             $table->timestamps();
